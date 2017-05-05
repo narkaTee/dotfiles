41db632810c7653e3deffe9cfd2b7a9fffce2c37
narkaTee <narkaTee@users.noreply.github.com>
2017-05-05 17:16:40 +0200

    Add tmux environment update

---
+++ bash/bashrc.d/tmux-env.bash
@@ -0,0 +1,11 @@
+#A dirty way to keep env vars up-to-date when re-attaching to a sess
+# Drawback: requires one prompt display before env change is effective
+if [ ! -z $TMUX ] && hash tmux 2>/dev/null; then
+    _refresh_env_from_tmux() {
+        if [ ! -z $TMUX ]; then
+            export "$(tmux show-environment | grep '^SSH_AUTH_SOCK')"
+        fi
+    }
+
+    PROMPT_COMMAND="_refresh_env_from_tmux;${PROMPT_COMMAND}"
+fi
