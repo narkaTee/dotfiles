0628932918c8dcd00ad09bd09d0e8d8709fd2ba4
narkaTee <narkaTee@users.noreply.github.com>
2017-07-07 10:48:53 +0200

    Move chef shell-init from profile to a function

---
+++ bash/bashrc.d/chef-init.bash
@@ -0,0 +1,11 @@
+# Convenience wrapper for chef shell-init.
+# it's too damn slow to put it in bash_profile...
+chef-init() {
+    # run chef shell-init if chef is installed
+    if hash chef 2>/dev/null; then
+        eval "$(chef shell-init bash)"
+    else
+        echo "can't find chef executeable!"
+        return 1
+    fi
+}
