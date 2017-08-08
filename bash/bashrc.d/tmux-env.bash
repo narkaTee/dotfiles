5a5681c63fc844f030cf36116e58afb0863cdd73
narkaTee <narkaTee@users.noreply.github.com>
2017-08-08 19:02:03 +0200

    More robust prompt command

--- bash/bashrc.d/tmux-env.bash
+++ bash/bashrc.d/tmux-env.bash
@@ -2,8 +2,14 @@
 # Drawback: requires one prompt display before env change is effective
 if [ ! -z $TMUX ] && hash tmux 2>/dev/null; then
     _refresh_env_from_tmux() {
-        if [ ! -z $TMUX ]; then
-            export "$(tmux show-environment | grep '^SSH_AUTH_SOCK')"
+        if [ -z $TMUX ]; then
+            # bail out for no tmux running
+            return;
+        fi
+
+        local tmux_env="$(tmux show-environment | grep '^SSH_AUTH_SOCK')"
+        if [ ! -z "$tmux_env" ]; then
+            export "$tmux_env"
         fi
     }
 
