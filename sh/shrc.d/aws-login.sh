e08cf94796ecbe4ef964ad8e6f555419409bbe19
narkaTee <narkaTee@users.noreply.github.com>
2025-03-14 15:15:14 +0100

    Add support for aws sso to aws-login helper

--- sh/shrc.d/aws-login.sh
+++ sh/shrc.d/aws-login.sh
@@ -16,9 +16,52 @@ aws_valid_credentials() {
     aws sts get-caller-identity > /dev/null 2>&1
 }
 
+aws_profile_contains_key_starting_with() {
+    profile="$1"
+    prefix="$2"
+    config_file="${AWS_CONFIG_FILE:-$HOME/.aws/config}"
+
+    if grep -q "^\[profile $profile\]" "$config_file"; then
+        awk -v profile="[profile $profile]" -v prefix="$prefix" '
+        # Check if the current line matches the profile
+        $0 == profile {
+            in_profile = 1;
+            next
+        }
+        # Check if the current line is a new profile section
+        /^\[.*\]/ {
+            in_profile = 0
+        }
+        # Check if the current line within the profile starts with the prefix
+        in_profile && $0 ~ "^" prefix {
+            found = 1;
+            exit
+        }
+        # Exit with the appropriate status based on whether the prefix was found
+        END {
+            exit !found
+        }
+        ' "$config_file"
+    else
+        return 1
+    fi
+}
+
+aws_use_azure_login_for_profile() {
+    aws_profile_contains_key_starting_with "$1" "azure_" \
+        && hash aws-azure-login 2> /dev/null
+}
+
+aws_use_sso_for_profile() {
+    aws_profile_contains_key_starting_with "$1" "sso_"
+}
+
 aws_try_login() {
     profile="$1"
-    if hash aws-azure-login 2> /dev/null; then
+    if aws_use_sso_for_profile "$profile"; then
+        echo "Trying to log in aws profile '$profile' with aws sso"
+        aws sso login
+    elif aws_use_azure_login_for_profile "$profile"; then
         echo "Trying to log into aws profile '$profile' with aws-azure-login"
         aws-azure-login --no-prompt
     else
