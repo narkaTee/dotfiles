476c7e98ceff460eb869180ae4f495d4798a72eb
narkaTee <narkaTee@users.noreply.github.com>
2025-09-12 17:17:19 +0200

    Add python venv halper

---
+++ sh/shrc.d/venv.sh
@@ -0,0 +1,46 @@
+# shellcheck shell=sh
+venv() {
+    venvSwitchTo="$1"
+    if [ -z "$venvSwitchTo" ]; then
+        if [ ! -d "$HOME/.venv/" ]; then
+            # shellcheck disable=SC2088
+            echo "~/.venv does not exist, create a venv with: python3 -m venv ~/.venv/<env name>"
+            return
+        fi
+
+        venv_list
+    else
+        if [ ! -d "$HOME/.venv/$venvSwitchTo" ]; then
+            echo "no such venv: $venvSwitchTo"
+
+            echo "available:"
+            venv_list
+
+            echo
+            echo "Or create it with: python3 -m venv ~/.venv/$venvSwitchTo"
+            return
+        fi
+
+        if [ ! -f "$HOME/.venv/$venvSwitchTo/bin/activate" ]; then
+            echo "no activate script found in $HOME/.venv/$venvSwitchTo/bin/activate"
+            return 1
+        fi
+
+        . "$HOME/.venv/$venvSwitchTo/bin/activate"
+    fi
+
+}
+
+venv_list() {
+    for env in "$HOME/.venv"/*; do
+        [ -d "$env" ] || continue
+        venvName=$(basename "$env")
+
+        printf "%s" "$venvName"
+        if [ "$VIRTUAL_ENV" = "$env" ]; then
+            echo " *"
+        else
+            echo
+        fi
+    done
+}
