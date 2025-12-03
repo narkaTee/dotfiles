64df3ee29e9dc24810d70a428a8d48ba2467282e
narkaTee <narkaTee@users.noreply.github.com>
2025-12-03 17:57:08 +0100

    Fix permanent ssh sock
    The refactoring for shellcheck broke the logic, doh!
    Also to properly work the snipped with the permanent socket needs to
    be at the end of the file otherweise the agent setup might overwrite
    SSH_AUTH_SOCK

--- sh/setup/ssh.sh
+++ sh/setup/ssh.sh
@@ -1,19 +1,11 @@
 # shellcheck shell=sh
-# Predictable ssh auth sock
-if [ -z "$SSH_AUTH_SOCK" ]; then
-    permanent="$HOME/.ssh_auth_sock"
-    if [ "$SSH_AUTH_SOCK" != "$permanent" ]; then
-        ln -sf "$SSH_AUTH_SOCK" "$permanent"
-        export SSH_AUTH_SOCK="$permanent"
-    fi
-fi
-
 setup_ssh_pagent() {
     eval "$(/usr/bin/ssh-pageant -q -r -a "/tmp/.ssh-pageant-$USERNAME")"
 }
 
 setup_cli_agent() {
     ssh-add -l > /dev/null 2>&1
+    # return code 2 means aent could not be contacted
     if [ "$?" -eq "2" ]; then
         test -r ~/.ssh-agent && . ~/.ssh-agent >/dev/null
 
@@ -40,5 +32,16 @@ else
     elif ! hash gnome-keyring-daemon 2>/dev/null; then
         # gnome keyring not found use cli tooling
         setup_cli_agent
+    fi
+fi
+
+# Predictable ssh auth sock, this enables the ssh agent forwarding to work when connecting to
+# an tmux session from an previous ssh session.
+# This needs to be at the end of the script to re-link the detected socket
+if [ -n "$SSH_AUTH_SOCK" ]; then
+    permanent="$HOME/.ssh_auth_sock"
+    if [ "$SSH_AUTH_SOCK" != "$permanent" ]; then
+        ln -sf "$SSH_AUTH_SOCK" "$permanent"
+        export SSH_AUTH_SOCK="$permanent"
     fi
 fi
