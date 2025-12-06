1966838e00ecca4c0898f37345f70fec268457d9
narkaTee <narkaTee@users.noreply.github.com>
2025-12-06 16:57:47 +0100

    Build additional fzf git helpers

--- sh/shrc.d/git_fzf.sh
+++ sh/shrc.d/git_fzf.sh
@@ -7,6 +7,10 @@ if hash fzf 2> /dev/null; then
         git branch "$@" --color=always --sort=-committerdate --sort=-HEAD --format=$'%(HEAD) %(color:yellow)%(refname:short) %(color:green)(%(committerdate:relative))\t%(color:blue)%(subject)%(color:reset)' | column -ts$'\t'
     }
 
+    __git_fzf_list_commits() {
+        git log --format="%C(auto)%h%d %s %C(green)%cr" --color=always
+    }
+
     case "$1" in
         branches)
             shift
@@ -16,12 +20,17 @@ if hash fzf 2> /dev/null; then
 
     # shellcheck disable=SC2120
     __git_fzf_branches() {
-        __git_fzf_list_branches "$@" | fzf \
-            -n1 \
-            --ansi \
-            --color hl:underline,hl+:underline \
+        __git_fzf_list_branches | fzf \
+            -q "$*" \
+            --nth 1..2 \
+            --border \
             --border-label "Branches" \
-            --bind "ctrl-b:reload(\"$__git_fzf_script\" branches -a)" \
+            --header "CTRL+b (all branches) CTRL+n (branches)" \
+            --ansi \
+            --tiebreak index \
+            --color hl:underline,hl+:underline \
+            --bind "ctrl-b:change-border-label(All Branches)+reload(\"$__git_fzf_script\" branches -a)" \
+            --bind "ctrl-n:change-border-label(Branches)+reload(\"$__git_fzf_script\" branches)" \
             --preview-window down,border-top,40% \
             --preview "git graph --color=always \$(cut -c3- <<< {} | cut -d' ' -f1) --" \
             | sed 's/^\* //' | sed 's/^  origin\///' | awk '{print $1}'
@@ -32,5 +41,19 @@ if hash fzf 2> /dev/null; then
         [ -n "$git_fzf_target_branch" ] && git switch "$git_fzf_target_branch"
     }
 
-    alias gb=__git_fzf_branch_switch
+    __git_fzf_commits() {
+        __git_fzf_list_commits | fzf \
+            -q "$*" \
+            --nth 2.. \
+            --accept-nth 1 \
+            --border \
+            --border-label "Commits" \
+            --ansi \
+            --tiebreak index \
+            --preview='git show --color=always --pretty="format:Author: %cn <%ce>%nDate: %cd%n---%n%s%n%n%b" --compact-summary -p {1}' \
+            | tr -d '\n' | copy
+    }
+
+    alias gbr=__git_fzf_branch_switch
+    alias gcs=__git_fzf_commits
 fi
