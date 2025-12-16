e6007bce0934fd414b0b19b48890f5bd3ca444dd
narkaTee <narkaTee@users.noreply.github.com>
2025-12-16 23:41:22 +0100

    Add kube debug helpers that use debug image

--- sh/shrc.d/container.sh
+++ sh/shrc.d/container.sh
@@ -4,3 +4,4 @@ alias mk=minikube
 alias mkd=minikube_docker_env
 alias kc=kubectl
 alias kctx="kubectl config use-context"
+alias kdbg="kubectl debug -it --share-processes --profile netadmin --image ghcr.io/narkatee/debug-container:latest"
