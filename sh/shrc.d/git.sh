80961d09216ce173616513607b8e518c4dc80af6
narkaTee <narkaTee@users.noreply.github.com>
2025-12-19 17:40:01 +0100

    Consolidate scripts

--- sh/shrc.d/git.sh
+++ sh/shrc.d/git.sh
@@ -4,3 +4,11 @@ alias gc="git co"
 alias gs="git s"
 alias gd="git d"
 alias gt="git t"
+
+#cd to git root
+cdgr() {
+    # _is_git function from the prompt.bash
+    # just bail out if we're not in a git repo
+    _is_git || return
+    cd "$(git rev-parse --show-toplevel)" || return
+}
